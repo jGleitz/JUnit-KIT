9fc2184d9e17b853d926d6ba66038d60e0499210
Martin Löper <martin.loeper@gmx.de>
2015-01-29 18:49:18 +0100

    (Incomplete) Test-Set for Sheet 6 Task C, Comment Improvement in Core-Module

--- src/sheet6/a_books/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Tests for Sheet 6 Task A: Index of words in a book. Run {@link sheet5.c_books.BooksInteractiveConsoleTest} to test
- * your implementation. Run {@link sheet5.c_books.OptionalBooksInteractiveConsoleTest} to run the same tests plus tests
- * for some bad commands your program should produce error messages for. This error handling is mandatory and 
- * requested by the Task.
- */
-package sheet6.a_books;
-
