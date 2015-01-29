d58f2a5ce9101fc78c6b0b0a1e4f9aedb235f046
Joshua Gleitze <git@joshuagleitze.de>
2015-01-29 22:42:40 +0100

    TaskSheet 6 C development

--- src/sheet6/c_bookDatabase/BookDatabaseTest.java
+++ src/sheet6/c_bookDatabase/BookDatabaseTest.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 
 import org.junit.AfterClass;
@@ -30,6 +32,8 @@ public class BookDatabaseTest extends InteractiveConsoleTest {
             "tITle=Grundkurs_Programmieren_iN_JAVA,year=2007",
             "Creator=Ralf_Reussner,year=2006"
     };
+    private String[] file;
+    private String line;
 
     /**
      * Fails the test as this test is incomplete an therefore does not grant anything. Remove this method as soon as the
@@ -75,48 +79,87 @@ public class BookDatabaseTest extends InteractiveConsoleTest {
      * <li>an empty file
      * <li>a file with general bad syntax
      * <li>a file with a malformed attribute name
-     * <li>a file with a malformed attribute value
      * </ul>
      */
     @Test
     public void testBadFile() {
-        String[] file;
-
         // empty file
-        file = new String[] {
-            ""
-        };
-        errorTest("quit", "0.3", getFile(file));
+        line = "";
+        errorTest("quit", "0.3", getFile(line));
 
         // random text
-        file = new String[] {
-            "Just some random text"
-        };
-        errorTest("quit", "0.3", getFile(file));
+        line = "Just some random text";
+        errorTest("quit", "0.3", getFile(line));
 
         // no value
-        file = new String[] {
-            "creator="
-        };
-        errorTest("quit", "0.3", getFile(file));
+        line = "creator=";
+        errorTest("quit", "0.3", getFile(line));
 
         // false attribute name
-        file = new String[] {
-            "creater=Max_Mustermann"
-        };
-        errorTest("quit", "0.3", getFile(file));
+        line = "creater=Max_Mustermann";
+        errorTest("quit", "0.3", getFile(line));
 
         // comma at the end
-        file = new String[] {
-            "creator=Max_Mustermann,"
-        };
-        errorTest("quit", "0.3", getFile(file));
+        line = "creator=Max_Mustermann,";
+        errorTest("quit", "0.3", getFile(line));
 
         // space in between
-        file = new String[] {
-            "creator=Max_Mustermann, title=Musterbuch"
-        };
-        errorTest("quit", "0.3", getFile(file));
+        line = "creator=Max_Mustermann, title=Musterbuch";
+        errorTest("quit", "0.3", getFile(line));
+    }
+
+    /**
+     * Tests the program's behaviour for a bad formed input file. Asserts that the program prints an error message for:
+     * <ul>
+     * <li>a malformed attribute value
+     * </ul>
+     * **/
+    @Test
+    public void testWrongInputFileAttributeValues() {
+        List<String> correctValues = new LinkedList<>();
+        List<String> falseValues = new LinkedList<>();
+        correctValues.add("creator=Max_Mustermann");
+        correctValues.add("year=2010");
+        correctValues.add("title=Musterbuch");
+
+        // bad character in creator
+        line = "creator=Max_Mus$termann";
+        errorTest("quit", "0.3", getFile(line));
+        falseValues.add(line);
+
+        // bad character in title
+        line = "title=Must&erbuch";
+        errorTest("quit", "0.3", getFile(line));
+        falseValues.add(line);
+
+        // year is not a number
+        line = "year=irgendetwas";
+        errorTest("quit", "0.3", getFile(line));
+        falseValues.add(line);
+
+        // year is a number, but not in range
+        line = "year=2020";
+        errorTest("quit", "0.3", getFile(line));
+        falseValues.add(line);
+
+        // year is a number, but not in range
+        line = "year=-100";
+        errorTest("quit", "0.3", getFile(line));
+        falseValues.add(line);
+
+        // test combinations of good and bad strings
+        List<String> goodOnes;
+        for (String bad : falseValues) {
+            goodOnes = new LinkedList<>();
+            for (String good : correctValues) {
+                if (!good.contains(bad.split("=")[0])) {
+                    goodOnes.add(good);
+                }
+            }
+            errorTest("quit", "0.3", getFile(goodOnes.get(0) + "," + bad));
+            errorTest("quit", "0.3", getFile(goodOnes.get(1) + "," + bad));
+            errorTest("quit", "0.3", getFile(goodOnes.get(0) + "," + bad + "," + goodOnes.get(1)));
+        }
     }
 
     @Override
@@ -139,6 +182,9 @@ public class BookDatabaseTest extends InteractiveConsoleTest {
         return result;
     }
 
+    /**
+     * Deletes all files that were created during the test run.
+     */
     @AfterClass
     public static void removeAllOpenedFiles() {
         for (String fileName : filesMap.keySet()) {
@@ -146,6 +192,14 @@ public class BookDatabaseTest extends InteractiveConsoleTest {
         }
     }
 
+    /**
+     * A message giving information about the input file used in a test.
+     * 
+     * @param commandLineArguments
+     *            The command line arguments the main method was called with during the test. The file message will read
+     *            the file name in the second argument and output the contents of the file its pointing to.
+     * @return A text representing the input file
+     */
     private static String fileMessage(String[] commandLineArguments) {
         String result = "";
         if (commandLineArguments.length > 1 && filesMap.containsKey(commandLineArguments[1])) {
@@ -155,6 +209,13 @@ public class BookDatabaseTest extends InteractiveConsoleTest {
         return result;
     }
 
+    /**
+     * Converts an Array of Strings into a String where each array element is represented as one line.
+     * 
+     * @param lines
+     *            The array to process
+     * @return the array as lines.
+     */
     private static String arrayToLines(String[] lines) {
         String result = "";
         for (String line : lines) {
@@ -166,7 +227,15 @@ public class BookDatabaseTest extends InteractiveConsoleTest {
         return result;
     }
 
-    private static String getFile(String[] lines) {
+    /**
+     * Returns a path to a file containing the lines given in {@code lines}. Creates the file if it was not created
+     * before.
+     * 
+     * @param lines
+     *            The lines to print in the file.
+     * @return path to a file containing {@code lines}
+     */
+    private static String getFile(String... lines) {
         String fileName = UUID.randomUUID().toString();
 
         if (!reverseFileMap.containsKey(lines)) {
