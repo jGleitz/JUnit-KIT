e8b75e8269bc77d81ba24b623d9f3adc476edf40
Joshua Gleitze <git@joshuagleitze.de>
2015-02-01 20:28:26 +0100

    6C: added high tolerance test

--- src/sheet6/c_bookDatabase/subtests/CompleteSearchScenarioTest.java
+++ src/sheet6/c_bookDatabase/subtests/CompleteSearchScenarioTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.fail;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.hamcrest.Matcher;
 import org.junit.Test;
@@ -21,40 +22,40 @@ import test.Input;
  * @since 31.01.2015
  */
 public class CompleteSearchScenarioTest extends BookDatabaseSubTest {
-	private String[] queries;
+    private String[] queries;
 
-	/**
-	 * Fails the test as this test is incomplete an therefore does not grant anything. Remove this method as soon as the
-	 * test reaches a certain degree of relevance.
-	 */
-	@Test
-	public void incomplete() {
-		fail("\n\nThis test is under development and incomplete!\n\n");
-	}
+    /**
+     * Fails the test as this test is incomplete an therefore does not grant anything. Remove this method as soon as the
+     * test reaches a certain degree of relevance.
+     */
+    @Test
+    public void incomplete() {
+        fail("\n\nThis test is under development and incomplete!\n\n");
+    }
 
-	/**
-	 * Simulates the Praktomat's public test. Asserts that:
-	 * <ul>
-	 * <li>the program outputs the expected search responses.
-	 * <li>an error message is printed for the last search command (key isbn not allowed)
-	 * </ul>
-	 */
-	@Test
-	public void publicPraktomatTest() {
-		file = new String[] {
-				"creator=galileocomputing,title=java_ist_auch_eine_insel",
-				"title=grundkursprogrammieren_in_java,year=2007",
-				"creator=ralf_reussner,year=2006"
-		};
-		
-		queries = new String[] {
-				"creator=ralf_reussner",
-				"year=2006",
-				"AND(creator=ralf_reussner,year=2006)",
-				"OR(creator=ralf_reussner,year=2006)",
-				"isbn=12345"
-		};
-		// @formatter:off
+    /**
+     * Simulates the Praktomat's public test. Asserts that:
+     * <ul>
+     * <li>the program outputs the expected search responses.
+     * <li>an error message is printed for the last search command (key isbn not allowed)
+     * </ul>
+     */
+    @Test
+    public void publicPraktomatTest() {
+        file = new String[] {
+                "creator=galileocomputing,title=java_ist_auch_eine_insel",
+                "title=grundkursprogrammieren_in_java,year=2007",
+                "creator=ralf_reussner,year=2006"
+        };
+
+        queries = new String[] {
+                "creator=ralf_reussner",
+                "year=2006",
+                "AND(creator=ralf_reussner,year=2006)",
+                "OR(creator=ralf_reussner,year=2006)",
+                "isbn=12345"
+        };
+        // @formatter:off
 		expectedResultMatchers = getMatchers(
 				is("creator=galileocomputing,title=java_ist_auch_eine_insel,year=unknown,false"),
 				is("creator=unknown,title=grundkursprogrammieren_in_java,year=2007,false"),
@@ -75,31 +76,31 @@ public class CompleteSearchScenarioTest extends BookDatabaseSubTest {
 				startsWith("Error,")
 		);
 		// @formatter:on
-		
-		multiLineTest(searchForAll(queries), expectedResultMatchers, "0.5", Input.getFile(file));
 
-		int randomTestRuns = 2;
-		for (int i = 0; i < randomTestRuns; i++) {
-			String[] shuffledCommands = searchForAll(shuffle(queries));
-			String[] shuffledFile = shuffleCase(file);
-			multiLineTest(shuffledCommands, expectedResultMatchers, "0.5", Input.getFile(shuffledFile));
-		}
-	}
+        multiLineTest(searchForAll(queries), expectedResultMatchers, "0.5", Input.getFile(file));
 
-	@Test
-	public void testAndOrAttributeValues() {
-		file = new String[] {
-				"creator=AND,title=OR",
-				"title=and,creator=or"
-		};
-		
-		queries = new String[] {
-				"creator=and",
-				"OR(creator=AND,title=and)",
-				"AND(creator=AND,title=OR)",
-				"OR(AND(creator=AND,title=OR),AND(title=and,creator=or))",
-		};
-		// @formatter:off
+        int randomTestRuns = 2;
+        for (int i = 0; i < randomTestRuns; i++) {
+            String[] shuffledCommands = searchForAll(shuffle(queries));
+            String[] shuffledFile = shuffleCase(file);
+            multiLineTest(shuffledCommands, expectedResultMatchers, "0.5", Input.getFile(shuffledFile));
+        }
+    }
+
+    @Test
+    public void testAndOrAttributeValues() {
+        file = new String[] {
+                "creator=AND,title=OR",
+                "title=and,creator=or"
+        };
+
+        queries = new String[] {
+                "creator=and",
+                "OR(creator=AND,title=and)",
+                "AND(creator=AND,title=OR)",
+                "OR(AND(creator=AND,title=OR),AND(title=and,creator=or))",
+        };
+        // @formatter:off
 		expectedResultMatchers = getMatchers(
 				is("creator=and,title=or,year=unknown,true"),
 				is("creator=or,title=and,year=unknown,false"),
@@ -114,23 +115,79 @@ public class CompleteSearchScenarioTest extends BookDatabaseSubTest {
 				is("creator=or,title=and,year=unknown,true")
 		);
 		// @formatter:on
-		
-		multiLineTest(searchForAll(queries), expectedResultMatchers, "0.00000000001", Input.getFile(file));
 
-		int randomTestRuns = 3;
-		for (int i = 0; i < randomTestRuns; i++) {
-			String[] shuffledCommands = searchForAll(shuffle(queries));
-			String[] shuffledFile = shuffleCase(file);
-			multiLineTest(shuffledCommands, expectedResultMatchers, "0.00000000001", Input.getFile(shuffledFile));
-		}
-	}
+        multiLineTest(searchForAll(queries), expectedResultMatchers, "0.00000000001", Input.getFile(file));
 
-	private String[] searchForAll(String[] queries) {
-		String[] result = new String[queries.length + 1];
-		for (int i = 0; i < queries.length; i++) {
-			result[i] = "search " + queries[i];
-		}
-		result[queries.length] = "quit";
-		return result;
-	}
+        int randomTestRuns = 3;
+        for (int i = 0; i < randomTestRuns; i++) {
+            String[] shuffledCommands = searchForAll(shuffle(queries));
+            String[] shuffledFile = shuffleCase(file);
+            multiLineTest(shuffledCommands, expectedResultMatchers, "0.00000000001", Input.getFile(shuffledFile));
+        }
+    }
+
+    @Test
+    public void testHighTolerance() {
+
+        queries = new String[] {
+                "title=" + UUID.randomUUID().toString(),
+                "AND(" + "creator=" + UUID.randomUUID().toString() + "," + "creator=" + UUID.randomUUID().toString()
+                        + ")",
+                and(and(and("creator=" + UUID.randomUUID().toString(), "creator=" + UUID.randomUUID().toString()),
+                        and("creator=" + UUID.randomUUID().toString(), "creator=" + UUID.randomUUID().toString())),
+                        and(and("title=" + UUID.randomUUID().toString(), "title=" + UUID.randomUUID().toString()),
+                                and("title=" + UUID.randomUUID().toString(), "title=" + UUID.randomUUID().toString()))),
+                "creator=a",
+                "year=0"
+        };
+        // @formatter:off
+                expectedResultMatchers = getMatchers(
+                        is("creator=galileocomputing,title=java_ist_auch_eine_insel,year=unknown,true"),
+                        is("creator=unknown,title=grundkurs_programmieren_in_java,year=2007,true"),
+                        is("creator=ralf_reussner,title=unknown,year=2006,false"),
+                        
+                        is("creator=galileocomputing,title=java_ist_auch_eine_insel,year=unknown,true"),
+                        is("creator=unknown,title=grundkurs_programmieren_in_java,year=2007,false"),
+                        is("creator=ralf_reussner,title=unknown,year=2006,true"),
+                        
+                        is("creator=galileocomputing,title=java_ist_auch_eine_insel,year=unknown,true"),
+                        is("creator=unknown,title=grundkurs_programmieren_in_java,year=2007,false"),
+                        is("creator=ralf_reussner,title=unknown,year=2006,false"),
+                        
+                        is("creator=galileocomputing,title=java_ist_auch_eine_insel,year=unknown,true"),
+                        is("creator=unknown,title=grundkurs_programmieren_in_java,year=2007,false"),
+                        is("creator=ralf_reussner,title=unknown,year=2006,true"),
+                        
+                        is("creator=galileocomputing,title=java_ist_auch_eine_insel,year=unknown,false"),
+                        is("creator=unknown,title=grundkurs_programmieren_in_java,year=2007,true"),
+                        is("creator=ralf_reussner,title=unknown,year=2006,true")
+                );
+                // @formatter:on
+
+        multiLineTest(searchForAll(queries), expectedResultMatchers, "1", Input.getFile(taskSheetInputFile));
+
+        int randomTestRuns = 2;
+        for (int i = 0; i < randomTestRuns; i++) {
+            String[] shuffledCommands = searchForAll(shuffle(queries));
+            String[] shuffledFile = shuffleCase(taskSheetInputFile);
+            multiLineTest(shuffledCommands, expectedResultMatchers, "1", Input.getFile(shuffledFile));
+        }
+    }
+
+    private String[] searchForAll(String[] queries) {
+        String[] result = new String[queries.length + 1];
+        for (int i = 0; i < queries.length; i++) {
+            result[i] = "search " + queries[i];
+        }
+        result[queries.length] = "quit";
+        return result;
+    }
+
+    private String and(String p1, String p2) {
+        return "AND(" + p1 + "," + p2 + ")";
+    }
+
+    private String or(String p1, String p2) {
+        return "OR(" + p1 + "," + p2 + ")";
+    }
 }
