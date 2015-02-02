c8a9a95fb9c7aa85393ab854ab3a4dd4bfc26e67
Joshua Gleitze <git@joshuagleitze.de>
2015-02-02 20:21:50 +0100

    assert that no divider is at end of string in containsExactlyDividedBy

--- src/test/KitMatchers.java
+++ src/test/KitMatchers.java
@@ -13,52 +13,53 @@ import org.hamcrest.Matcher;
  */
 public class KitMatchers {
 
-	/**
-	 * Creates a matcher that matches if the examined String contains exactly the elements in the specified String
-	 * Array, separated exactly by {@code divider}.
-	 * 
-	 * @param substrings
-	 *            the substrings that the returned matcher will expect to be contained by any examined string
-	 * @param divider
-	 *            the divider String that the returned matcher will expect the Strings of {@code substrings} to be
-	 *            divided by in any examined string
-	 */
-	public static Matcher<String> containsExactlyDividedBy(final String[] substrings, final String divider) {
-		return new BaseMatcher<String>() {
+    /**
+     * Creates a matcher that matches if the examined String contains exactly the elements in the specified String
+     * Array, separated exactly by {@code divider}.
+     * 
+     * @param substrings
+     *            the substrings that the returned matcher will expect to be contained by any examined string
+     * @param divider
+     *            the divider String that the returned matcher will expect the Strings of {@code substrings} to be
+     *            divided by in any examined string
+     */
+    public static Matcher<String> containsExactlyDividedBy(final String[] substrings, final String divider) {
+        return new BaseMatcher<String>() {
 
-			@Override
-			public boolean matches(final Object testObject) {
-				String testString = (String) testObject;
-				boolean contains = true;
-				int length = 0;
-				for (int i = 0; i < substrings.length && contains; i++) {
-					String sub = substrings[i];
-					length += sub.length();
-					int index = testString.indexOf(sub) + sub.length();
-					contains = (index >= sub.length());
-					if (contains) {
-						boolean isAtEnd = (index == testString.length());
-						boolean nextIsDivider = (!isAtEnd && testString.substring(index, index + 1).equals(divider));
-						if (nextIsDivider) {
-							testString = testString.substring(0, index) + testString.substring(index + 1);
-						}
-						contains = (isAtEnd || nextIsDivider);
-					}
-				}
-				return contains && (testString.length() == length);
-			}
+            @Override
+            public boolean matches(final Object testObject) {
+                String testString = (String) testObject;
+                boolean contains = true;
+                int length = 0;
+                for (int i = 0; i < substrings.length && contains; i++) {
+                    String sub = substrings[i];
+                    length += sub.length();
+                    int index = testString.indexOf(sub) + sub.length();
+                    contains = (index >= sub.length());
+                    if (contains) {
+                        boolean isAtEnd = (index == testString.length());
+                        boolean nextIsDivider = (!isAtEnd && testString.substring(index, index + 1).equals(divider));
+                        nextIsDivider = nextIsDivider && testString.length() > index + 1;
+                        if (nextIsDivider) {
+                            testString = testString.substring(0, index) + testString.substring(index + 1);
+                        }
+                        contains = (isAtEnd || nextIsDivider);
+                    }
+                }
+                return contains && (testString.length() == length);
+            }
 
-			@Override
-			public void describeTo(Description description) {
-				description.appendText("A String containing exactly ").appendValueList("", ", ", "", substrings);
-			}
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("A String containing exactly ").appendValueList("", ", ", "", substrings);
+            }
 
-			@Override
-			public void describeMismatch(final Object item, final Description description) {
-				description.appendText("was ").appendValue((String) item);
-			}
+            @Override
+            public void describeMismatch(final Object item, final Description description) {
+                description.appendText("was ").appendValue((String) item);
+            }
 
-		};
-	}
+        };
+    }
 
 }
