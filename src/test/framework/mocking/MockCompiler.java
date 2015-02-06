dd3d81ba253c4086e6cb48f311125ae65f575a2f
Joshua Gleitze <git@joshuagleitze.de>
2015-02-06 20:58:56 +0100

    get Java Compiler on Windows
    I'm on Ubuntu so I can't test. But this COULD enable us to use the Java
    compiler on Windows, too.

--- src/test/framework/mocking/MockCompiler.java
+++ src/test/framework/mocking/MockCompiler.java
@@ -1,9 +1,13 @@
 package test.framework.mocking;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.util.Arrays;
 
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
+
+import test.framework.FrameworkException;
 
 /**
  * Helper class to compile a {@link MockerJavaSourceFile}.
@@ -13,7 +17,7 @@ import javax.tools.ToolProvider;
  * @since 04.02.2015
  */
 public abstract class MockCompiler {
-    private static final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+    private static final JavaCompiler compiler = getJavaCompiler();
     private static final MockCompilerFileManager fileManager = new MockCompilerFileManager(
             compiler.getStandardFileManager(null, null, null));
 
@@ -30,4 +34,42 @@ public abstract class MockCompiler {
         return fileManager.pollCompiledMocker(sourceFile.getName());
     }
 
+    private static JavaCompiler getJavaCompiler() {
+        JavaCompiler comp = ToolProvider.getSystemJavaCompiler();
+        if (comp == null) {
+            boolean isWindows = System.getProperty("os.name").startsWith("Windows");
+            if (isWindows) {
+                File javaProgramFolder = new File("C:\\Program Files\\Java");
+                if (!javaProgramFolder.exists()) {
+                    throw new FrameworkException("Your system does not provide a Java Compiler. It's Windows but we "
+                            + "can't find your Java installation!");
+                }
+                FileFilter jdkFilter = new FileFilter() {
+                    @Override
+                    public boolean accept(File file) {
+                        return file.isDirectory() && file.getName().startsWith("jdk-1.");
+                    }
+                };
+                File[] jdkFolders = javaProgramFolder.listFiles(jdkFilter);
+                for (File f : jdkFolders) {
+                    System.setProperty("java.home", f.getAbsolutePath());
+                    comp = ToolProvider.getSystemJavaCompiler();
+                    if (comp != null) {
+                        break;
+                    }
+                }
+                if (comp == null && jdkFolders.length == 0) {
+                    throw new FrameworkException("Your system does not provide a Java Compiler. It's Windows, but "
+                            + "we can't find a JDK!");
+                } else if (comp == null) {
+                    throw new FrameworkException("Your system does not provide a Java Compiler. It's Windows and "
+                            + "we did find " + jdkFolders.length + " JDKs. Still, no compiler!");
+                }
+            } else {
+                throw new FrameworkException("Your System does not provide a Java Compiler and is not Windows – "
+                        + "we're all out of ideas!");
+            }
+        }
+        return comp;
+    }
 }
