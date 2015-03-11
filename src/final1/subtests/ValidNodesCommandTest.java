59382ad0035717d647488864c845272cc6a83f45
pcworld <0188801@gmail.com>
2015-03-11 22:36:27 +0100

    final1: Add task sheet recommend test

--- src/final1/subtests/ValidNodesCommandTest.java
+++ src/final1/subtests/ValidNodesCommandTest.java
@@ -1,7 +1,11 @@
 package final1.subtests;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.fail;
 
+import java.util.List;
+
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 import test.Input;
@@ -14,24 +18,37 @@ import test.Input;
  */
 public class ValidNodesCommandTest extends RecommendationSubtest {
 
-    private static final String TASK_SHEET_NODES
-            = "calc:202,centos5:105,centos6:106,centos7:107,impress:203,libreoffice:200,officesuite,operatingsystem,software,writer:201";
-    
     /**
      * Asserts that the {@code nodes} command performs as expected for the example given on the task sheet.
      */
     @Test
     public void taskSheetExampleTest() {
-        command = "nodes";
-        expectedResult = TASK_SHEET_NODES;
-        oneLineTest(addQuit(command), expectedResult, Input.getFile(TASK_SHEET_INPUT_FILE));
+        testAgainstTaskSheet(TASK_SHEET_INPUT_FILE);
     }
     
     @Test
     public void spacesTest() {
-        command = "nodes";
-        expectedResult = TASK_SHEET_NODES;
-        oneLineTest(addQuit(command), expectedResult, Input.getFile(TASK_SHEET_INPUT_FILE));
+        testAgainstTaskSheet(TASK_SHEET_INPUT_FILE_SPACES);
+    }
+    
+    private void testAgainstTaskSheet(String[] input) {
+        String[] queries = new String[] {
+                "nodes",
+                "recommend S1 105",
+                "recommend S3 107",
+                "recommend UNION(S1 105,S3 107)",
+                "recommend S1 201",
+                "recommend UNION(S1 201,INTERSECTION(S1 105,S3 107))",
+        };
+        List<Matcher<String>> matchers = getMatchers(
+                is("calc:202,centos5:105,centos6:106,centos7:107,impress:203,libreoffice:200,officesuite,operatingsystem,software,writer:201"),
+                is("centos6:106,centos7:107"),
+                is("centos5:105,centos6:106"),
+                is("centos5:105,centos6:106,centos7:107"),
+                is("calc:202,impress:203,libreoffice:200"),
+                is("calc:202,centos6:106,impress:203,libreoffice:200")
+        );
+        multiLineTest(addQuit(queries), matchers, Input.getFile(input));
     }
     
     @Test
