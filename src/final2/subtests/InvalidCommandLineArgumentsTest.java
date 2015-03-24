9ec5ddbf8c4f5f0636edff96af2a8c8e4089940c
jGleitz <git@joshuagleitze.de>
2015-03-24 21:40:33 +0100

    Added invalid rules

--- src/final2/subtests/InvalidCommandLineArgumentsTest.java
+++ src/final2/subtests/InvalidCommandLineArgumentsTest.java
@@ -95,6 +95,8 @@ public class InvalidCommandLineArgumentsTest extends LangtonSubtest {
 		errorTest("quit", Input.getFile(TASK_SHEET_INPUT_FILE_1), "rule=--270-90-315-45-90");
 		errorTest("quit", Input.getFile(TASK_SHEET_INPUT_FILE_1), "rule=-270-90--315-45-90");
 		errorTest("quit", Input.getFile(TASK_SHEET_INPUT_FILE_1), "rule={270,90,315,45,90}");
+		errorTest("quit", Input.getFile(TASK_SHEET_INPUT_FILE_1), "rule=270-0-315-45-90");
+		errorTest("quit", Input.getFile(TASK_SHEET_INPUT_FILE_1), "rule=270-180-315-45-90");
 
 		errorTest("quit", Input.getFile(TASK_SHEET_INPUT_FILE_1),
 			"rule=270-90-9999999999999999999999999999999999999-45-90");
