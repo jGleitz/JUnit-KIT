419eaadca9faa464f3b883a2a7c7c87d998dcae1
jGleitz <git@joshuagleitze.de>
2015-03-24 22:43:05 +0100

    changed something, can't remember what.

--- src/final2/subtests/MoveTest.java
+++ src/final2/subtests/MoveTest.java
@@ -15,7 +15,18 @@ public class MoveTest extends LangtonSubtest {
 
 	/**
 	 * Asserts that {@code move} works on a very simple, easy to understand example: The rule is set to
-	 * {@code 90-90-90-90-90} and we have only one ant on a {@code 2x2} board.
+	 * {@code 90-90-90-90-90} and we have only one ant on this 2x2 board:
+	 * 
+	 * <pre>
+	 * <code>
+	 * 0a
+	 * 00
+	 * </code>
+	 * </pre>
+	 * 
+	 * The ant should run through the board clockwise, changing all colours to {@code 3} in the first four steps. In the
+	 * next four steps, the ant should run clockwise once again, changing all colours back to {@code 1} and leaving the
+	 * board in its initial state.
 	 */
 	@Test
 	public void simpleMoveTest() {
