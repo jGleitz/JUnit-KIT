4761d4e565e725d4b44117c51095936d08f02b3d
jGleitz <git@joshuagleitze.de>
2015-03-26 16:37:31 +0100

    Made system exit status check in ErrorOrNoOutputRun optional

--- src/test/runs/ErrorOrNoOutputRun.java
+++ src/test/runs/ErrorOrNoOutputRun.java
@@ -13,21 +13,31 @@ import test.TestObject;
  */
 public class ErrorOrNoOutputRun implements Run {
 	private String command;
+	private boolean checkSystemExitStatus;
 
 	/**
 	 * Constructs a test run for the given {@code command} that shall result in either an error message or no output at
 	 * all.
+	 * 
+	 * @param command
+	 *            The command to run
+	 * @param mustTerminate
+	 *            {@code true} if the program has to terminate after the run. Enables system exit status checking.
 	 */
-	public ErrorOrNoOutputRun(String command) {
+	public ErrorOrNoOutputRun(String command, boolean mustTerminate) {
 		this.command = command;
+		this.checkSystemExitStatus = mustTerminate;
 	}
 
 	/**
 	 * Constructs a test run without a command that shall result in either an error message or no output at all. Use
 	 * only to test errors before the first command.
+	 * 
+	 * @param mustTerminate
+	 *            {@code true} if the program has to terminate after the run. Enables system exit status checking.
 	 */
-	public ErrorOrNoOutputRun() {
-		this(null);
+	public ErrorOrNoOutputRun(boolean mustTerminate) {
+		this(null, mustTerminate);
 	}
 
 	/*
@@ -39,12 +49,17 @@ public class ErrorOrNoOutputRun implements Run {
 	public void check(String[] testedClassOutput, String errorMessage) {
 		if (testedClassOutput.length > 0) {
 			new ErrorRun(command).check(testedClassOutput, errorMessage);
-			assertThat(errorMessage + "\nYour class printed an error message. This requires System.exit(1)!",
-				TestObject.getLastMethodsSystemExitStatus(), suits(SystemExitStatus.EXACTLY.status(1), true));
+			if (checkSystemExitStatus) {
+				assertThat(errorMessage + "\nYour class printed an error message. This requires System.exit(1)!",
+					TestObject.getLastMethodsSystemExitStatus(), suits(SystemExitStatus.EXACTLY.status(1), true));
+			}
 		} else {
 			new NoOutputRun(command).check(testedClassOutput, errorMessage);
-			assertThat(errorMessage + "\nYour class did not print an error message. It may only call System.exit(0)!",
-				TestObject.getLastMethodsSystemExitStatus(), suits(SystemExitStatus.WITH_0, false));
+			if (checkSystemExitStatus) {
+				assertThat(errorMessage
+						+ "\nYour class did not print an error message. It may only call System.exit(0)!",
+					TestObject.getLastMethodsSystemExitStatus(), suits(SystemExitStatus.WITH_0, false));
+			}
 		}
 	}
 
