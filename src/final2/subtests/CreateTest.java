f201c14d5239242076e3e6d18cbb59671351eac1
Roman Langrehr <roman.langrehr@yahoo.de>
2015-03-27 16:35:09 +0100

    - Added test for nortwards looking ants - More detailed checks in simpleCreateTest - Bugfixes

--- src/final2/subtests/CreateTest.java
+++ src/final2/subtests/CreateTest.java
@@ -23,6 +23,79 @@ public class CreateTest extends LangtonSubtest {
 		setAllowedSystemExitStatus(SystemExitStatus.WITH_0);
 	}
 
+	public void createNorthAnt() {
+		inputFile = new String[] {
+				"0000",
+				"0000",
+				"0a00",
+				"0000"
+		};
+		runs = new Run[] {
+				new ExactRun("ant", is("a")),
+				new NoOutputRun("create B,1,1"),
+				new ExactRun("ant", is("a,b")),
+				new ExactRun("position b", is("1,1")),
+				new ExactRun("direction b", is("N")),
+				new ExactRun("field 1,1", is("b")),
+				quit()
+		};
+		sessionTest(runs, Input.getFile(inputFile));
+
+		inputFile = new String[] {
+				"b000",
+				"0000",
+				"0a00",
+				"Z000"
+		};
+		runs = new Run[] {
+				new ExactRun("ant", is("a,b,z")),
+				new NoOutputRun("create H,1,3"),
+				new ExactRun("ant", is("a,b,h,z")),
+				new ExactRun("position h", is("1,3")),
+				new ExactRun("direction h", is("N")),
+				new ExactRun("field 1,3", is("b")),
+				quit()
+		};
+		sessionTest(runs, Input.getFile(inputFile));
+	}
+
+	@Test
+	public void recyclePlaceTest() {
+		inputFile = new String[] {
+				"0000",
+				"0b00",
+				"0a00",
+				"0000"
+		};
+		runs = new Run[] {
+				new NoOutputRun("escape b"),
+				new NoOutputRun("create c,1,1"),
+				new ExactRun("ant", is("a,c")),
+				new ExactRun("field 1,1", is("c")),
+				new ExactRun("direction c", is("S")),
+				new ExactRun("position c", is("1,1")),
+				quit()
+		};
+		sessionTest(runs, Input.getFile(inputFile));
+
+		inputFile = new String[] {
+				"0000",
+				"0000",
+				"0a00",
+				"0000"
+		};
+		runs = new Run[] {
+				new NoOutputRun("move 1"),
+				new NoOutputRun("create b,2,1"),
+				new ExactRun("ant", is("a,b")),
+				new ExactRun("field 2,1", is("b")),
+				new ExactRun("direction b", is("S")),
+				new ExactRun("position b", is("2,1")),
+				quit()
+		};
+		sessionTest(runs, Input.getFile(inputFile));
+	}
+
 	/**
 	 * Asserts that the command create can reuse the name of ants, that have left the playing field.
 	 */
@@ -63,43 +136,6 @@ public class CreateTest extends LangtonSubtest {
 		sessionTest(runs, Input.getFile(inputFile));
 	}
 
-	@Test
-	public void recyclePlaceTest() {
-		inputFile = new String[] {
-				"0000",
-				"0b00",
-				"0a00",
-				"0000"
-		};
-		runs = new Run[] {
-				new NoOutputRun("escape b"),
-				new NoOutputRun("create c,1,1"),
-				new ExactRun("ant", is("a,b")),
-				new ExactRun("field 1,1", is("b")),
-				new ExactRun("direction b", is("S")),
-				new ExactRun("position b", is("1,1")),
-				quit()
-		};
-		sessionTest(runs, Input.getFile(inputFile));
-
-		inputFile = new String[] {
-				"0000",
-				"0000",
-				"0a00",
-				"0000"
-		};
-		runs = new Run[] {
-				new NoOutputRun("move 1"),
-				new NoOutputRun("create b,2,1"),
-				new ExactRun("ant", is("a,b")),
-				new ExactRun("field 2,1", is("b")),
-				new ExactRun("direction b", is("S")),
-				new ExactRun("position b", is("2,1")),
-				quit()
-		};
-		sessionTest(runs, Input.getFile(inputFile));
-	}
-
 	/**
 	 * Asserts that command 'create ant,x,y' works on simple examples.
 	 */
@@ -115,9 +151,11 @@ public class CreateTest extends LangtonSubtest {
 				new ExactRun("ant", is("a")),
 				new NoOutputRun("create b,1,1"),
 				new ExactRun("ant", is("a,b")),
+				new ExactRun("position b", is("1,1")),
+				new ExactRun("direction b", is("S")),
+				new ExactRun("field 1,1", is("b")),
 				quit()
 		};
-
 		sessionTest(runs, Input.getFile(inputFile));
 
 		inputFile = new String[] {
@@ -130,9 +168,11 @@ public class CreateTest extends LangtonSubtest {
 				new ExactRun("ant", is("a,b,z")),
 				new NoOutputRun("create h,1,3"),
 				new ExactRun("ant", is("a,b,h,z")),
+				new ExactRun("position h", is("1,3")),
+				new ExactRun("direction h", is("S")),
+				new ExactRun("field 1,3", is("h")),
 				quit()
 		};
-
 		sessionTest(runs, Input.getFile(inputFile));
 	}
 }
